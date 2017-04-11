a8911ba7a7218266a480c925bb72db4d4b98678c
Gitea <bezkom@gmail.com>
2017-04-11 10:19:16 -0400

    mantra #2

--- mantra.py
+++ mantra.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import math
 import cairo
+from math import pow
+from colorsys import hls_to_rgb
+from random import randint as r
+from math import fsum as s
 
-WIDTH, HEIGHT = 720, 480
-
+WIDTH, HEIGHT = 1280, 720
 
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
@@ -14,18 +17,38 @@ ctx.rectangle(0, 0, 1, 1)
 ctx.set_source(white)
 ctx.fill()
 
+xpos = []
+ypos = []
 
-dx = 1.0/11.0
-dy = 1.0/11.0
+N = 11
 
-for i in range(1, 8):
-    for j in range(1, 8):
+XMUL = 1.9
+YMUL = 1.6
 
-        col = cairo.SolidPattern(i/10.0, 1-j/10.0, i*j/100.0)
+XTABLE = [pow(XMUL, i) for i in range(1, 9)]
+YTABLE = [pow(YMUL, j) for j in range(1, 9)]
+CTABLE = [hls_to_rgb(c/16.0, .5, 1) for c in range(16)]
+
+XSEQ = [XTABLE[r(0, 7)] for i in range(N)]
+YSEQ = [YTABLE[r(0, 7)] for j in range(N)]
+
+XMAX = s(XSEQ)
+YMAX = s(YSEQ)
+
+
+y = 0
+for j in range(N):
+    dy = YSEQ[j]/YMAX
+    x = 0
+    for i in range(N):
+        ij = (i+j) % N
+        rgb = CTABLE[r(0, 15)]
+        col = cairo.SolidPattern(rgb[0], rgb[1], rgb[2])
         ctx.set_source(col)
-        x = i/9.0
-        y = j/9.0
+        dx = XSEQ[ij]/XMAX
         ctx.rectangle(x, y, dx, dy)
         ctx.fill()
+        x += dx
+    y += dy
 
-surface.write_to_png('mantra.png')
+surface.write_to_png('mantra2.png')
